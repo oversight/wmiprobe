85234e96279f3919a83290037a9a78b5bbb7783a
Jeroen van der Heijden <jeroen@cesbit.com>
2022-04-06 09:59:42 +0200

    Fix

--- wmiprobe.py
+++ wmiprobe.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    cl.setup_logger(args.log_level, args.log_colorized)
+    AgentCoreClient.setup_logger(args.log_level, args.log_colorized)
 
     cl = AgentCoreClient(
         'wmiProbe',
